f7004ba1b898a43302b0a101cda636f2947a77dc
leonardog9823 <guanuco43@gmail.com>
2022-07-03 04:32:11 -0300

    cambio por /account

--- Auth/urls.py
+++ Auth/urls.py
@@ -1,8 +1,10 @@
-from django.urls import path
+from django.urls import path, reverse_lazy
 from Auth import views
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path("login/", views.login_user, name="login"),
     path("register/", views.register_user, name="register"),
     path("edit_profile/", views.edit_profile, name="edit_profile"),
+    path("logout/", LogoutView.as_view(), {'next_page': reverse_lazy("login")}, name='logout')
 ]
--- Blog/settings.py
+++ Blog/settings.py
@@ -126,6 +126,7 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_URL = "/auth/login"
+LOGOUT_REDIRECT_URL = '/inicio'
 
 MEDIA_URL  = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
